cd21e92d1b002b7c30b74e8a03bc4bd03d040fe6
mkouichi <kic.mhr.qs@gmail.com>
2023-10-05 16:01:11 -0400

    Route guard

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,11 @@ const router = createBrowserRouter([
       {
         path: '/register',
         element: <Register />,
+      },
+      {
+        path: '/new-ticket',
+        element: <PrivateRoute />,
+        children: [{ index: true, element: <NewTicket /> }],
       },
     ],
   },
