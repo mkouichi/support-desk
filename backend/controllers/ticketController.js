139e054736874a0711f9b2e9f5e4d73fe0cd7190
mkouichi <kic.mhr.qs@gmail.com>
2023-10-05 14:27:14 -0400

    Get a single ticket, update and delete ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,6 +1,5 @@
 const asyncHandler = require('express-async-handler');
 
-const User = require('../models/userModel');
 const Ticket = require('../models/ticketModel');
 
 // @desc    Get user tickets
@@ -10,6 +9,25 @@ const getTickets = asyncHandler(async (req, res) => {
   const tickets = await Ticket.find({ user: req.user.id }); // req.user.id comes from the authMiddleware.js
 
   res.status(200).json(tickets);
+});
+
+// @desc    Get user ticket
+// @route   GET /api/tickets/:id
+// @access  Private
+const getTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  // Reject if user is not the owner of the ticket
+  if (ticket.user.toString() !== req.user.id) {
+    return res.status(401).json(new Error('Not Authorized'));
+  }
+
+  res.status(200).json(ticket);
 });
 
 // @desc    Create new ticket
@@ -33,4 +51,56 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+// @desc    Update ticket
+// @route   PUT /api/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  // Reject if user is not the owner of the ticket
+  if (ticket.user.toString() !== req.user.id) {
+    return res.status(401).json(new Error('Not Authorized'));
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
+// @desc    Delete ticket
+// @route   DELETE /api/tickets/:id
+// @access  Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  // Reject if user is not the owner of the ticket
+  if (ticket.user.toString() !== req.user.id) {
+    return res.status(401).json(new Error('Not Authorized'));
+  }
+
+  await Ticket.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ success: true });
+});
+
+module.exports = {
+  getTickets,
+  getTicket,
+  createTicket,
+  updateTicket,
+  deleteTicket,
+};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { getTickets, createTicket } = require('../controllers/ticketController');
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+  updateTicket,
+  deleteTicket,
+} = require('../controllers/ticketController');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .put(protect, updateTicket)
+  .delete(protect, deleteTicket);
 
 module.exports = router;
