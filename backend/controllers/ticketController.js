95fc71fcf55394eb7b7d329fa72deacc2b24e247
mkouichi <kic.mhr.qs@gmail.com>
2023-10-05 12:26:30 -0400

    Get tickets and create ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,30 @@ const Ticket = require('../models/ticketModel');
 // @route   GET /api/tickets/
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get tickets' });
+  const tickets = await Ticket.find({ user: req.user.id }); // req.user.id comes from the authMiddleware.js
+
+  res.status(200).json(tickets);
 });
 
 // @desc    Create new ticket
 // @route   POST /api/tickets/
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Create tickets' });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please add a product and description');
+  }
+
+  const ticket = await Ticket.create({
+    user: req.user.id, // req.user.id comes from the authMiddleware.js
+    product,
+    description,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = { getTickets, createTicket };
