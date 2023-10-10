a2023b1aa4303b68213fb52d55594ddcbfc96a8c
mkouichi <kic.mhr.qs@gmail.com>
2023-10-10 19:05:18 -0400

    Create ticket functionality & BackButton

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -4,11 +4,35 @@ import ticketService from './ticketService';
 const initialState = {
   tickets: [],
   ticket: {},
-  isError: false,
-  isSuccess: false,
   isLoading: false,
+  isSuccess: false,
+  isError: false,
   message: '',
 };
+
+// Create new ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      // Get token from state
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      console.error(error);
+      console.error(message);
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const ticketSlice = createSlice({
   name: 'ticket',
@@ -16,7 +40,21 @@ export const ticketSlice = createSlice({
   reducers: {
     reset: (state) => initialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTicket.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = ticketSlice.actions;
