e536e803af8e63423b44ee811726897d26bb8ef8
mkouichi <kic.mhr.qs@gmail.com>
2023-10-12 16:19:55 -0400

    - Display notes - Add timestamps to noteModel

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -4,12 +4,21 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
+import NoteItem from '../components/NoteItem';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
 function Ticket() {
-  const { ticket, isLoading, isError, message } = useSelector(
-    (state) => state.tickets
+  const {
+    ticket,
+    isLoading: ticketIsLoading,
+    isError,
+    message,
+  } = useSelector((state) => state.tickets);
+
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
   );
 
   const dispatch = useDispatch();
@@ -22,6 +31,7 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
   }, [dispatch, isError, message, ticketId]);
 
   const onTicketClose = () => {
@@ -30,7 +40,7 @@ function Ticket() {
     navigate('/tickets');
   };
 
-  if (isLoading) return <Spinner />;
+  if (ticketIsLoading || notesIsLoading) return <Spinner />;
   if (isError) return <h3>Something went wrong</h3>;
 
   return (
@@ -52,7 +62,14 @@ function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note}>
+          {note.text}
+        </NoteItem>
+      ))}
 
       {ticket.status !== 'closed' && (
         <button className='btn btn-block btn-danger' onClick={onTicketClose}>
