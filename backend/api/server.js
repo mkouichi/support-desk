c1ea5f574913c8ad43ec2aae596991f64eeec0cb
mkouichi <kic.mhr.qs@gmail.com>
2024-01-10 15:12:04 -0500

    Change folder structure

--- backend/api/server.js
+++ backend/api/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 require('dotenv').config();
 const colors = require('colors');
 const PORT = process.env.PORT || 5000;
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const { errorHandler } = require('../middleware/errorMiddleware');
+const connectDB = require('../config/db');
 
 // Connect to database
 connectDB();
@@ -15,17 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/tickets', require('./routes/ticketRoutes'));
+app.use('/api/users', require('../routes/userRoutes'));
+app.use('/api/tickets', require('../routes/ticketRoutes'));
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
   // Fix app crashing on refresh in deployment
   app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
   });
 } else {
   app.get('/', (req, res) => {
